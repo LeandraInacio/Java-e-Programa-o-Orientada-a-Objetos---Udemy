8b596b2024cef292c0a33d8b632d863e56450bc7
LeandraInacio <leandrainaciodepaula@gmail.com>
2021-11-08 14:44:12 -0300

    Teste para saída de dados

---
+++ saidaDe Dados/src/SaidaDeDados.java
@@ -0,0 +1,25 @@
+
+public class SaidaDeDados {
+
+	public static void main(String[] args) {
+		
+		String product1 ="Computer";
+		String product2 =" Office desk";
+		
+		int age =30;
+		int code =5290;
+		String gender = "F";
+		
+		float price = 2100.0f;
+		float price2 = 650.50f;
+		float measure = 53.234567f;
+		
+		
+		System.out.println(" Products: \n" + product1 +" which price is $ "+price
+				+"\n"+ product2+" , wich price is $ "+price2+"  \n \n Record: "+age +" years old,"
+				+ "code "+code +"  and gender: "+gender+" \n Measure with eight decimal places: "
+				+measure);
+
+	}
+
+}
