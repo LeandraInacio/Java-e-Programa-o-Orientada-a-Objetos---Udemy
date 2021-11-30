d6b27d754bfa8463fdbd9ceb369ec6bc5ac95184
leandrainaciodepaula@gmail.com <leandrainaciodepaula@gmail.com>
2021-11-30 11:42:26 -0300

    Aula PipeLine

---
+++ Funções Lambda/aulaPipeLine/src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		List<Integer> lista = Arrays.asList(3, 4, 5, 10, 7);
+
+		Stream<Integer> st1 = lista.stream().map(p -> p * 10);
+		System.out.println(Arrays.toString(st1.toArray()));
+
+		int sum = lista.stream().reduce(0, (x, y) -> x + y);
+		System.out.println("Soma: " + sum);
+
+		List<Integer> newList = lista.stream()
+				.filter(x -> x % 2 == 0)
+				.map(x -> x * 10)
+				.collect(Collectors.toList());
+		System.out.println(Arrays.toString(newList.toArray()));
+		
+
+	}
+
+}
