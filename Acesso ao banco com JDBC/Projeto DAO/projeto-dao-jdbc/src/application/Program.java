dda14eff1c83fd942eba5da20f3e48e9f48a87df
leandrainaciodepaula@gmail.com <leandrainaciodepaula@gmail.com>
2021-12-06 14:13:03 -0300

    Implementando o findAll

--- Acesso ao banco com JDBC/Projeto DAO/projeto-dao-jdbc/src/application/Program.java
+++ Acesso ao banco com JDBC/Projeto DAO/projeto-dao-jdbc/src/application/Program.java
@@ -13,13 +13,19 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println("========TESTE FINDBYID ========");		
+		System.out.println("========TESTE 1 FINDBYID ========");		
 		Seller seller = sellerDao.findById(3);		
 		System.out.println(seller);
 		
-		System.out.println("\n========TESTE FINDBYDEPARTMENT ========");
+		System.out.println("\n========TESTE 2 FINDBYDEPARTMENT ========");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
+		for(Seller obj : list) {
+			System.out.println("\n"+obj);
+		}
+		
+		System.out.println("\n========TESTE 3 FINDBYALL ========");
+		list = sellerDao.findAll();
 		for(Seller obj : list) {
 			System.out.println("\n"+obj);
 		}
