5a104ea5ef0abc1454a79b993c3bc1a30cf29bf5
leandrainaciodepaula@gmail.com <leandrainaciodepaula@gmail.com>
2021-12-09 10:45:46 -0300

    Atualização de dependências e properties

--- Projeto web services com spring boot e JPA-Hibernate/projeto1/src/main/java/com/cursojavaudemy/projeto1/resources/UserResource.java
+++ Projeto web services com spring boot e JPA-Hibernate/projeto1/src/main/java/com/cursojavaudemy/projeto1/resources/UserResource.java
@@ -14,7 +14,7 @@ public class UserResource {
 
 	@GetMapping	
 	public ResponseEntity<User> findAll(){
-		 User u = new User(1L,"Maria","Maria@gmail.com","999999","senha");
+		 User u = new User(1L,"Maria","Maria@gmail.com","9999-9999","senha");
 		 return ResponseEntity.ok().body(u);
 	}
 	
